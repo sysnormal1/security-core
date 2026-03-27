97bce4ef011d9c450a29ac1d6c155183a019faab
ti-jumbo <jumbo.ti@jumboalimentos.com.br>
2026-03-27 11:14:42 -0300

    updated jwt core service

--- src/main/java/com/sysnormal/security/core/security_core/services/jwt/JwtCoreService.java
+++ src/main/java/com/sysnormal/security/core/security_core/services/jwt/JwtCoreService.java
@@ -11,6 +11,9 @@ import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
@@ -21,35 +24,87 @@ public class JwtCoreService {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtCoreService.class);
 
+    private String publicPemFilePath;
+
     private JwtParser jwtParser = null;
 
     private String publicPem;
     private PublicKey publicKey;
 
-    protected JwtCoreService() {};
+    protected JwtCoreService() {
+        logger.debug("INIT {}.{} {}",this.getClass().getSimpleName(), "JwtCoreService()");
+        logger.debug("END {}.{} {}",this.getClass().getSimpleName(), "JwtCoreService()");
+    };
 
     protected JwtCoreService(String publicPem) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        this.publicPem = publicPem;
-        this.publicKey = KeyUtils.parseRsaPublicKey(publicPem);
-        buildJwtParser(this.publicKey);
+        logger.debug("INIT {}.{}",this.getClass().getSimpleName(), "JwtCoreService(publicPem)");
+        this.setPublicPem(publicPem);
+        logger.debug("END {}.{}",this.getClass().getSimpleName(), "JwtCoreService(publicPem)");
     };
 
     protected JwtCoreService(PublicKey publicKey) {
-        this.publicKey = publicKey;
-        buildJwtParser(this.publicKey);
+        logger.debug("INIT {}.{}",this.getClass().getSimpleName(), "JwtCoreService(publicKey)");
+        this.setPublicKey(publicKey);
+        logger.debug("END {}.{}",this.getClass().getSimpleName(), "JwtCoreService(publicKey)");
     };
 
+    public void setPublicPemFilePath(String publicPemFilePath) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+        logger.debug("INIT {}.{}",this.getClass().getSimpleName(), "setPublicPemFilePath");
+        this.publicPemFilePath = publicPemFilePath;
+        logger.info("setted public key file path: '{}'", this.publicPemFilePath);
+        if (TextUtils.hasNotNullText(this.publicPemFilePath)) {
+            Path path = Path.of(this.publicPemFilePath);
 
+            logger.debug("Resolved public key path to absolute path: '{}'", path.toAbsolutePath());
+
+            if (!Files.exists(path)) {
+                logger.error("Public key file does not exist at path: '{}'", path.toAbsolutePath());
+                throw new IllegalStateException("Public key file not found: " + path.toAbsolutePath());
+            }
+
+            if (!Files.isReadable(path)) {
+                logger.error("Public key file is not readable at path: '{}'", path.toAbsolutePath());
+                throw new IllegalStateException("Public key file is not readable: " + path.toAbsolutePath());
+            }
+
+            logger.info("Public key file found. Attempting to read file...");
+
+            String pem = Files.readString(path);
+            this.setPublicPem(pem);
+        }
+        logger.debug("END {}.{}",this.getClass().getSimpleName(), "setPublicPemFilePath");
+    }
+
+    public void setPublicPem(String publicPem) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        logger.debug("INIT {}.{}",this.getClass().getSimpleName(), "setPublicPem");
+        this.publicPem = publicPem;
+        logger.info("Successfully read public key file ({} bytes).", publicPem.length());
+        PublicKey publicKey = KeyUtils.parseRsaPublicKey(publicPem);
+        logger.info("Successfully parsed RSA public key.");
+        this.setPublicKey(publicKey);
+        logger.debug("END {}.{}",this.getClass().getSimpleName(), "setPublicPem");
+    }
+
+    public void setPublicKey(PublicKey publicKey) {
+        logger.debug("INIT {}.{}",this.getClass().getSimpleName(), "setPublicKey");
+        this.publicKey = publicKey;
+        buildJwtParser(this.publicKey);
+        logger.info("JWT parser successfully initialized.");
+        logger.debug("END {}.{}",this.getClass().getSimpleName(), "setPublicKey");
+    }
 
     public void buildJwtParser(PublicKey publicKey) {
+        logger.debug("INIT {}.{}",this.getClass().getSimpleName(), "buildJwtParser");
         if (publicKey != null) {
             jwtParser = Jwts.parser()
                     .verifyWith(publicKey)
                     .build();
         }
+        logger.debug("END {}.{}",this.getClass().getSimpleName(), "buildJwtParser");
     }
 
     public Claims getClaims(String token) {
+        logger.debug("INIT {}.{}",this.getClass().getSimpleName(), "getClaims");
         Claims result = jwtParser
                 .parseSignedClaims(token)
                 .getPayload();
@@ -61,10 +116,12 @@ public class JwtCoreService {
                 result.get("accessProfileId"),
                 result.getExpiration() != null ? (result.getExpiration().getTime() - System.currentTimeMillis()) / 1000 : 0
         );
+        logger.debug("END {}.{}",this.getClass().getSimpleName(), "getClaims");
         return result;
     }
 
     public Long getExpiration(String token) {
+        logger.debug("INIT {}.{}",this.getClass().getSimpleName(), "getExpiration");
         logger.debug("token: {}", token);
         if (!TextUtils.hasNotNullText(token)) return null;
         Claims claims = getClaims(token);
@@ -77,11 +134,13 @@ public class JwtCoreService {
             logger.warn("JWT sem claim 'exp'");
             return null;
         }
+        logger.debug("END {}.{}",this.getClass().getSimpleName(), "getExpiration");
         return ((Number) expObj).longValue();
 
     }
 
     public DefaultDataSwap checkToken(String token){
+        logger.debug("INIT {}.{}",this.getClass().getSimpleName(), "checkToken");
         DefaultDataSwap result = new DefaultDataSwap();
         try {
             Long expiresIn = getExpiration(token); //seconds
@@ -119,6 +178,7 @@ public class JwtCoreService {
             result.message = "unexpected error";
             result.setException(e);
         }
+        logger.debug("END {}.{}",this.getClass().getSimpleName(), "checkToken");
         return result;
     }
 
--- src/main/java/com/sysnormal/security/core/security_core/utils/KeyUtils.java
+++ src/main/java/com/sysnormal/security/core/security_core/utils/KeyUtils.java
@@ -1,5 +1,8 @@
 package com.sysnormal.security.core.security_core.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
@@ -11,7 +14,10 @@ import java.util.Base64;
 
 public final class KeyUtils {
 
+    private static final Logger logger = LoggerFactory.getLogger(KeyUtils.class);
+
     public static PublicKey parseRsaPublicKey(String pem) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        logger.debug("INIT {}.{}",KeyUtils.class.getSimpleName(), "parseRsaPublicKey");
         String publicKeyContent = pem
                 .replace("-----BEGIN PUBLIC KEY-----", "")
                 .replace("-----END PUBLIC KEY-----", "")
@@ -21,11 +27,12 @@ public final class KeyUtils {
 
         X509EncodedKeySpec spec = new X509EncodedKeySpec(decoded);
         KeyFactory kf = KeyFactory.getInstance("RSA");
-
+        logger.debug("end {}.{}",KeyUtils.class.getSimpleName(), "parseRsaPublicKey");
         return kf.generatePublic(spec);
     }
 
     public static PrivateKey parseRsaPrivateKey(String pem) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        logger.debug("INIT {}.{}",KeyUtils.class.getSimpleName(), "parseRsaPrivateKey");
         String privateKeyContent = pem
                 .replace("-----BEGIN PRIVATE KEY-----", "")
                 .replace("-----END PRIVATE KEY-----", "")
@@ -35,7 +42,7 @@ public final class KeyUtils {
 
         PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
         KeyFactory kf = KeyFactory.getInstance("RSA");
-
+        logger.debug("END {}.{}",KeyUtils.class.getSimpleName(), "parseRsaPrivateKey");
         return kf.generatePrivate(spec);
     }
 
